2462dd9bb41b2ee5d88b401df5706ee988807ec0
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-10-07 08:45:01 +0200

    WIP

--- multideps/src/main/scala/multideps/config/VersionsConfig.scala
+++ multideps/src/main/scala/multideps/config/VersionsConfig.scala
@@ -3,7 +3,6 @@ package multideps.config
 import moped.json.JsonDecoder
 import moped.json.JsonString
 import moped.json.ValueResult
-import moped.json.JsonArray
 import moped.json.JsonObject
 import moped.json.ErrorResult
 import moped.reporters.Diagnostic
@@ -12,22 +11,29 @@ import moped.json.DecodingResult
 import moped.json.SelectMemberCursor
 
 final case class VersionsConfig(
-    default: String = "",
-    extras: Map[String, String] = Map.empty
+    default: JsonString = JsonString(""),
+    extras: Map[String, JsonString] = Map.empty
 )
 
 object VersionsConfig {
+  def apply(default: String): VersionsConfig =
+    VersionsConfig(JsonString(default))
+  def apply(default: String, extras: Map[String, String]): VersionsConfig =
+    VersionsConfig(
+      JsonString(default),
+      extras.mapValues(JsonString(_)).toMap
+    )
   implicit lazy val default = VersionsConfig()
   implicit lazy val encoder = moped.macros.deriveEncoder[VersionsConfig]
   implicit lazy val decoder = new JsonDecoder[VersionsConfig] {
     def decode(context: DecodingContext): DecodingResult[VersionsConfig] = {
       context.json match {
-        case JsonString(value) => ValueResult(VersionsConfig(value))
+        case s: JsonString => ValueResult(VersionsConfig(s))
         case obj @ JsonObject(members) =>
           obj.value.get("default") match {
-            case Some(JsonString(default)) =>
+            case Some(default: JsonString) =>
               val results = (obj.value - "default").map {
-                case (label, JsonString(version)) =>
+                case (label, version: JsonString) =>
                   ValueResult(label -> version)
                 case (label, other) =>
                   ErrorResult(Diagnostic.typeMismatch("JsonString", context))
