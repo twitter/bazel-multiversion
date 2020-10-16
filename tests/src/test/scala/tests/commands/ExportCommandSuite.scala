7d5fe55eeb0769f74c4c243f7daeef2c82e4cc9b
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-10-16 17:58:23 +0200

    WIP

--- tests/src/test/scala/tests/commands/ExportCommandSuite.scala
+++ tests/src/test/scala/tests/commands/ExportCommandSuite.scala
@@ -1,6 +1,8 @@
 package tests.commands
 
 import munit.TestOptions
+import moped.testkit.FileLayout
+import moped.internal.console.Utils
 
 class ExportCommandSuite extends tests.BaseSuite {
 
@@ -39,6 +41,7 @@ class ExportCommandSuite extends tests.BaseSuite {
             .mkString("\n")
         assertNoDiff(obtainedQuery, expectedQuery)
       }
+      pprint.log(FileLayout.asString(workingDirectory.resolve("src")))
       if (buildQuery.nonEmpty) {
         app().process("bazel", "build", buildQuery).call()
       }
@@ -61,12 +64,12 @@ class ExportCommandSuite extends tests.BaseSuite {
 
   def scalaLibrary(name: String, code: String): String =
     s"""|/src/$name
-        |$code
+        |${code.stripSuffix("\n") + "\n"}
         |/src/BUILD
-        |load("@io_bazel_rules_scala//scala:scala_library.bzl", "scala_library")
+        |load("@io_bazel_rules_scala//scala:scala.bzl", "scala_library")
         |scala_library(
         |  name = "src",
-        |  srcs = ["MyApp.scala"],
+        |  srcs = ["$name"],
         |)
         |""".stripMargin
 
@@ -75,9 +78,8 @@ class ExportCommandSuite extends tests.BaseSuite {
     s"""|  - dependency: com.lihaoyi:fansi_2.12:0.2.8
         |    versionScheme: pvp
         |  - dependency: com.lihaoyi:pprint_2.12:0.5.6
-        |/src/BUILD
         |${scalaLibrary("MyApp.scala", "object MyApp")}
         |""".stripMargin,
-    expectedQuery = "src/..."
+    buildQuery = "//src/..."
   )
 }
