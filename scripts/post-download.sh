6be1a85fd8ac5024f919dfc92397d0bbb52dc52b
Eugene Yokota <eed3si9n@gmail.com>
2021-01-05 20:01:11 -0500

    post-download script

---
+++ scripts/post-download.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+rm target/dist-macOS.zip 2> /dev/null || true
+rm target/dist-Linux.zip 2> /dev/null || true
+cp $HOME/Downloads/dist-macOS.zip target/dist-macOS.zip
+cp $HOME/Downloads/dist-Linux.zip target/dist-Linux.zip
+cd target/
+unzip dist-macOS.zip
+chmod +x multiversion
+mv multiversion multiversion-x64_64-apple-darwin
+unzip dist-Linux.zip
+chmod +x multiversion
+mv multiversion multiversion-x64_64-pc-linux
+cd ..
