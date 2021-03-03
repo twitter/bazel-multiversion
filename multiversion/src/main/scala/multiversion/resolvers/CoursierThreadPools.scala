fb3fa133cc1efa0ca767d5274ff8633d2be79d2c
Eugene Yokota <eed3si9n@gmail.com>
2021-03-03 13:39:41 -0500

    Reduce parallelism to 2 for testing

--- multiversion/src/main/scala/multiversion/resolvers/CoursierThreadPools.scala
+++ multiversion/src/main/scala/multiversion/resolvers/CoursierThreadPools.scala
@@ -12,7 +12,7 @@ class CoursierThreadPools {
 
   // From https://github.com/johnynek/bazel-deps/blob/dbd90f155e45e0b2529999d0b74ea65b49e6fb07/src/scala/com/github/johnynek/bazel_deps/CoursierResolver.scala#L19
   val downloadPool: ExecutorService = Executors.newFixedThreadPool(
-    12,
+    2,
     new ThreadFactory {
       val defaultThreadFactory = Executors.defaultThreadFactory()
       def newThread(r: Runnable) = {
